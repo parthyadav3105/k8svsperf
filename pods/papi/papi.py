c4867c32c46a7e4c46586fff664bcba81f20f44a
Parth Yadav <parth.yadav@ramanujan.du.ac.in>
2020-05-04 03:13:35 +0530

    W.I.P. Patch for K8s pod deployment @VSperf

--- pods/papi/papi.py
+++ pods/papi/papi.py
@@ -1,7 +1,28 @@
+# Copyright 2020 University Of Delhi.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 """
 Automation of Pod Deployment with Kubernetes Python API
 """
+
+import os
+import logging
+from jinja2 import Environment, meta, Template
+
+from conf import settings as S
+from pods.pod.pod import IPod
 
 class IPodPapi(IPod):
     """
@@ -13,13 +34,60 @@ class IPodPapi(IPod):
         """
         super(IPodPapi, self).__init__()
         name, ext = os.path.splitext(S.getValue('LOG_FILE_POD'))
+        name = name + str(self._number)
+        rename_logs = "{name}_{uid}{ex}".format(name=name,
+                                                uid=S.getValue('LOG_TIMESTAMP'),
+                                                ex=ext)
+        self._logger = logging.getLogger(__name__)
+        self._logfile = os.path.join(S.getValue('RESULT_PATH'), rename_logs)
+        self._podfile = os.path.join(S.getValue('RESULT_PATH'),
+                                     "{name}.yaml".format(name=name))
+        self._podtemplate = S.getValue('POD_TEMPLATE')
 
     def create(self):
         """
         Creation Process
         """
+        self._create_pod_definition_file()
+        self._cmd = ['sudo', 'kubectl', 'create',
+                     '-f='+self._podfile]
+        self._logger.info("Creating POD %d ...", self._number)
+        super(IPodPapi, self).start()
 
-    def destroy(self):
+    def terminate(self):
         """
         Cleanup Process
         """
+        self._cmd = ['sudo', 'kubectl', 'delete',
+                     '-f='+self._podfile]
+        self._logger.info("Terminating POD %d ...", self._number)
+        super(IPodPapi, self).start()
+
+    def _get_values(self):
+        """
+        Returns values of template_varriables as dict
+        """
+        values = dict()
+        with open(self._podtemplate) as templatefile:
+            env = Environment()
+            template = env.parse(templatefile.read())
+            template_variables = meta.find_undeclared_variables(template)
+        for var in template_variables:
+            values[var] = S.getValue(var)
+        return values
+
+    def _render_template(self):
+        """
+        Renders yaml templates
+        """
+        with open(self._podtemplate) as templatefile:
+            template = Template(templatefile.read())
+        return template.render(self._get_values())
+
+    def _create_pod_definition_file(self):
+        """
+        Generates pod yaml definition file from pod template
+        """
+        with open(self._podfile, "w") as podfile:
+            pod_definition = self._render_template()
+            podfile.write(pod_definition)
--- pods/pod/pod.py
+++ pods/pod/pod.py
@@ -1,9 +1,22 @@
+# Copyright 2020 University Of Delhi.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """
 Interface for POD
 """
 
-import time
-import pexpect
 from tools import tasks
 
 class IPod(tasks.Process):
@@ -19,7 +32,11 @@ class IPod(tasks.Process):
         """
         Initialization Method
         """
-        raise NotImplementedError()
+        self._number = IPod._number_pods
+        self._logger.debug('Initializing %s. Pod with index %s',
+                           self._number + 1, self._number)
+        IPod._number_pods = IPod._number_pods + 1
+        self._log_prefix = 'pod_%d_cmd : ' % self._number
 
     def create(self):
         """
